af7a4aec10555068495d07f1cd6e737793ba5c63
Harshvardhan Malpani <2611741+harshvardhanmalpani@users.noreply.github.com>
2018-06-11 22:31:24 +0530

    init from Gist

---
+++ console-commands/scrapeEmails.js
@@ -0,0 +1,24 @@
+mails=$('.BltHke.nH.oy8Mbf[role="main"] .yX.xY .yW span');
+var emmm=[];var isspam=0;
+var allowed=["cpanel","harshmalpani","tutes.club"];
+mails.each(function(){
+var thismail=$(this).attr('email');
+isspam=0;
+$.each(allowed,function(j,b){
+if(thismail.indexOf(b) > -1){
+	isspam=0;
+	return false;
+	}
+else {
+	isspam=1;
+}
+});
+if(isspam)emmm.push(thismail);
+});
+emmm=$.unique(emmm);
+var fst='';
+$.each(emmm,function(i,v){
+fst+=v+' OR ';
+});
+fst=fst.slice(0,-4);
+console.log(fst);
